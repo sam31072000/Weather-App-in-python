b3f799312a71e9aa790f2afc3651c078a00b01f4
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2020-03-15 23:48:46 +0530

    Update pythonweatherapp.py

--- pythonweatherapp.py
+++ pythonweatherapp.py
@@ -1,18 +1,13 @@
 import requests , time , threading
 from win10toast import ToastNotifier
-#ipapi is used to get the current ip address with location
-ipinfo = requests.get("https://ipapi.co/json").json()   #to req from url and json is used to covert from decimal format
+ipinfo = requests.get("https://ipapi.co/json").json()   #ipapi is used to get the current ip address with location
 city = ipinfo['city']
 weatherapi = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=6b70db340cd49ddb47c6d2e7445222f5"
-
 def showweather():
     weather = requests.get(weatherapi).json()
     condition = weather["weather"][0]["description"]
     temp = weather["main"]["temp"] - 273.15
     notif = ToastNotifier()
-    #print(weather)
-    #print(f"The weather {condition} and the temp is {temp} centigrade")
     notif.show_toast(f"The Current Weather of {city} is",f"Its's {condition}, currently temperature is {temp} centigrade")  #show notifications
     threading.Timer(1000*3600,showweather).start()   #show after 1 hr and to stop ctr+c
-
 showweather()
